1b14ddfe0dec108bf541116556d0c12d77f345e9
liamtg <liamgilchrist@gmail.com>
2023-05-18 15:55:43 +1000

    sidebar v1 incorporated

--- tools/web/world/urls.py
+++ tools/web/world/urls.py
@@ -3,9 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("map/", views.map_view, name="map_view"),
-    path ("sidebar/", views.map_Sidebar_temp, name="map_Sidebar_temp"),
-    
+    path("map/", views.map_view, name="map_view"),    
     path("poi_geojson/", views.poi_geojson, name="poi_geojson"),
     path(
         "linefeature_geojson/",
